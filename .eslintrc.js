42e0a2b4db66bd92e787d255459c736d2e311ef1
ste163 <samtedwards@pm.me>
2021-07-17 11:15:01 -0400

    added jest; change file structure; removed boilerplate vue app

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
