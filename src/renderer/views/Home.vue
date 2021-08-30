aa47cf0ceb445f96f0119e19e953db94490372a0
ste163 <samtedwards@pm.me>
2021-08-30 14:54:59 -0400

    converted file structure

---
+++ src/renderer/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="home">HOME</div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+  },
+  pluginOptions: {
+    electronBuilder: {
+      preload: "src/main/preload.ts",
+      // Use this to change the entrypoint of your app's main process
+      mainProcessFile: "src/main/background.ts",
+      // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
+      rendererProcessFile: "src/renderer/main.ts",
+    },
+  },
+};
