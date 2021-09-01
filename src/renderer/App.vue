74992c7e07315fc7bb4f46ca2e2cd87987a7ca67
ste163 <samtedwards@pm.me>
2021-08-31 20:00:10 -0400

    getting list of all column divs

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,14 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 <template>
   <!-- TODO -->
-  <!-- Columns will be templates -->
+  <!-- Move all column logic & saving into a custom hook(s) -->
+  <!-- Style Template Columns that use Slots -->
   <!-- Allow columns to be created when clicking a nav item -->
   <!-- Allow columns to be resizable -->
-  <!-- Allow columns to be dragged and dropped -->
+  <!-- DONE - Allow columns to be dragged and dropped -->
+  <!-- Allow columns to be ordered in their dropzone -->
+  <!-- Only show drop-zones when the user is dragging a column -->
   <!-- Allow columns to be replaced when a new one is clicked (ie, its content changing) -->
   <!-- Allow columns to be pinned/locked -->
   <the-sidebar />
-
-  <!-- If the drop-zones are empty, only show them when the user is dragging a column -->
 
   <!-- Left drop-able area -->
   <div
@@ -18,11 +20,16 @@
     @dragover.prevent
   >
     <div
-      v-for="column in getColumn(1)"
+      v-for="(column, i) in getColumn(1)"
+      :ref="
+        (el) => {
+          if (el) this.leftColumnDivs[i] = el;
+        }
+      "
       :key="column.id"
       class="column-draggable"
       draggable="true"
-      @dragstart="onDragStart($event, column)"
+      @dragstart="onColumnDragStart($event, column)"
     >
       {{ column.header }}
     </div>
@@ -40,11 +47,16 @@
     @dragover.prevent
   >
     <div
-      v-for="column in getColumn(2)"
+      v-for="(column, i) in getColumn(2)"
+      :ref="
+        (el) => {
+          if (el) this.rightColumnDivs[i] = el;
+        }
+      "
       :key="column.id"
       class="column-draggable"
       draggable="true"
-      @dragstart="onDragStart($event, column)"
+      @dragstart="onColumnDragStart($event, column)"
     >
       {{ column.header }}
     </div>
@@ -52,14 +64,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onBeforeUpdate, ref } from "vue";
 import TheSidebar from "./components/TheSidebar.vue";
 
 export default defineComponent({
   components: { TheSidebar },
 
-  // TODO: IColumn interface, because we'll be storing the user's layout
   setup() {
+    // TODO: IColumn interface, because we'll be storing the user's layout
     const columns = ref([
       {
         id: 0,
@@ -70,33 +82,51 @@ export default defineComponent({
       { id: 2, header: "Lexicons", dropzone: 2 },
     ]);
 
+    // References to the DOM elements
+    const leftColumnDivs = ref<Array<HTMLDivElement>>([]);
+    const rightColumnDivs = ref<Array<HTMLDivElement>>([]);
+
+    // Going to need to access column being dragged by its Header and check
+    // against its html inner text
+    const currentlyDraggedColumnId = ref<number>();
+
+    // Returns the ref of what column --- when I have the interface built, I can add that as the return type
     function getColumn(dropzone: number) {
+      // Return only the columns with the matching dropzone number
       return columns.value.filter((column) => column.dropzone === dropzone);
     }
 
-    // Item needs to be an interface with something with JUST an id, like IHasId
-    // onDragStart can be moved into a hook because it will just require items to have an ID using the IHasId interface
-    // Or maybe it's just a type of anything with an id
-    function onDragStart(event: DragEvent, item: any) {
-      console.log(item);
+    // Move this to a Types directory
+    type ColumnId = {
+      id: number;
+    };
+
+    function onColumnDragStart(event: DragEvent, item: ColumnId): void {
       if (event.dataTransfer !== null) {
+        console.log("RIGHT COLUMN DIVS", rightColumnDivs.value);
+        console.log("LEFT COLUMN DIVS", leftColumnDivs.value);
         event.dataTransfer.dropEffect = "move";
         event.dataTransfer.effectAllowed = "move";
-        event.dataTransfer.setData("itemId", item.id);
+        event.dataTransfer.setData("itemId", item.id.toString());
+
+        currentlyDraggedColumnId.value = item.id;
+        console.log("COLUMN ID", currentlyDraggedColumnId.value);
       } else {
         console.error("Event was not a drag event.");
       }
     }
 
-    // onColumnDrop is currently only for the columns. Which isn't a problem. We can have a drop for the columns
-    // and then a drop for list items
-    function onColumnDrop(event: DragEvent, dropzone: number) {
+    function onColumnDrop(event: DragEvent, dropzone: number): void {
+      console.log("Dropping item into", dropzone);
       if (event.dataTransfer !== null) {
         const itemId = event.dataTransfer.getData("itemId");
         const column = columns.value.find(
           (column) => column.id === parseInt(itemId)
         );
 
+        const afterElement = getDragAfterElement(dropzone, event.clientY);
+
+        // Moves the column into the correct dropzone
         if (column !== undefined) {
           column.dropzone = dropzone;
         }
@@ -105,10 +135,37 @@ export default defineComponent({
       }
     }
 
+    // Need a sorting function for the dragging. Otherwise, it gets dumped into the right location, but not ordered in any way
+    function getDragAfterElement(dropzone: number, y: number) {
+      const allColumnsInDropzone = getColumn(dropzone);
+      const draggableElements = allColumnsInDropzone.filter(
+        (column) => column.id !== currentlyDraggedColumnId.value
+      );
+      console.log(
+        "All Draggable elements except for currently dragged",
+        draggableElements
+      );
+
+      // draggableElements.reduce(
+      //   (closestElement, childElements) => {
+      //     // const box = childElements.getBoundingClientRect();
+      //     // console.log(box);
+      //   },
+      //   { offset: Number.NEGATIVE_INFINITY }
+      // );
+    }
+
+    onBeforeUpdate(() => {
+      leftColumnDivs.value = [];
+      rightColumnDivs.value = [];
+    });
+
     return {
       getColumn,
-      onDragStart,
+      onColumnDragStart,
       onColumnDrop,
+      leftColumnDivs,
+      rightColumnDivs,
     };
   },
 });
