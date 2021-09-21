2f06ba38fd06748094a11660ab98d8fab54a0511
ste163 <samtedwards@pm.me>
2021-09-21 15:48:50 -0400

    setup basic modal structure with teleport

--- src/renderer/components/BaseModal.vue
+++ src/renderer/components/BaseModal.vue
@@ -1,12 +1,71 @@
 <template>
-  <div>
-    MODAL
-    <!-- Header slot -->
-    <!-- Main content slot -->
-  </div>
+  <teleport to="#modal-container">
+    <div v-if="isModalActive" class="modal-background">
+      <div class="modal-card">
+        <div class="modal-card-header">
+          <h2>
+            <slot name="header">Header</slot>
+          </h2>
+
+          <button-close
+            @click="emitCloseModal"
+            class="modal-card-header-close"
+          />
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+  </teleport>
 </template>
 
-<!--
-  Props for:
-  isModalActive
--->
+<script setup lang="ts">
+import ButtonClose from "./ButtonClose.vue";
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  isModalActive: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits(["closeModal"]);
+
+function emitCloseModal(): void {
+  emit("closeModal");
+}
+</script>
+
+<style scoped>
+.modal-background {
+  position: absolute;
+  z-index: 100;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(3px);
+}
+
+.modal-card {
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  background-color: var(--white);
+  padding: 0.25em;
+  height: 100px;
+  min-width: 10em;
+  max-width: fit-content;
+  border-radius: 5px;
+}
+
+.modal-card-header {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.modal-card-header-close {
+  padding-top: 0.26em;
+}
+</style>
--- src/renderer/components/TheColumnProject.vue
+++ src/renderer/components/TheColumnProject.vue
@@ -1,5 +1,22 @@
 <template>
   <div>Project Column Content</div>
+  <button @click="isModalActive = !isModalActive">Create</button>
+
+  <base-modal
+    :is-modal-active="isModalActive"
+    @close-modal="isModalActive = false"
+  >
+    <template v-slot:header>Create Project</template>
+
+    TEST MODAL</base-modal
+  >
 </template>
 
 <!-- Need to inject the store so we can track global project state -->
+
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseModal from "./BaseModal.vue";
+
+const isModalActive = ref<boolean>(false);
+</script>
