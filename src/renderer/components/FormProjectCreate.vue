eac63d737b44cc5d302f985122e43a6fe1bb77c6
ste163 <samtedwards@pm.me>
2021-09-24 13:34:23 -0400

    added column sub heading component

--- src/renderer/components/FormProjectCreate.vue
+++ src/renderer/components/FormProjectCreate.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="container">
+  <div class="form-container">
     <!-- Must have a single root node to animate with <transition-group /> -->
-    <button-back class="back-button" @click="emitGoBack" />
+
+    <div class="form-container-header">
+      <button-back class="back-button" @click="emitGoBack" />
+
+      <column-sub-heading class="form-title"
+        >Create New Project</column-sub-heading
+      >
+    </div>
+
     <form class="form" @submit.prevent="onSubmit">
       <input-text name="title" type="text" label="Title" />
 
@@ -19,6 +27,7 @@
 import { useForm } from "vee-validate";
 import { toFormValidator } from "@vee-validate/zod";
 import { z } from "zod";
+import ColumnSubHeading from "./ColumnSubHeading.vue";
 import InputText from "./InputText.vue";
 import ButtonSubmit from "./ButtonSubmit.vue";
 import ButtonBack from "./ButtonBack.vue";
@@ -56,12 +65,23 @@ const onSubmit = handleSubmit((values, { resetForm }) => {
 </script>
 
 <style scoped>
-.container {
+.form-container {
   width: clamp(180px, 90%, 300px);
+}
+
+.form-container-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5em;
 }
 
 .back-button {
   margin: 0.5em 0em 0.5em 0.5em;
+  padding-top: 0.22em;
+}
+
+.form-title {
+  margin-left: 1em;
 }
 
 .form {
