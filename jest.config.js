928dee6473a6858840d83b9a446e804fd921a8eb
ste163 <samtedwards@pm.me>
2021-09-27 18:52:55 -0400

    added ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
-// jest.config.js
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = async () => {
-  return {
-    verbose: true,
-    haste: {
-      // When I get the warning for having two haste-maps, try and change the path to the one in the error message
-      // hasteImplModulePath: "./package.json",
-    },
-  };
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
 };
