1f0030e3b5e5ecd9d88663df10a470bd392dd538
ste163 <samtedwards@pm.me>
2021-11-13 13:23:26 -0500

    added tiptap

--- packages/renderer/src/views/Writer.vue
+++ packages/renderer/src/views/Writer.vue
@@ -1,3 +1,119 @@
+<script setup lang='ts'>
+import { useEditor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+
+const editor = useEditor({
+	content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+	extensions: [
+		StarterKit,
+	],
+});
+
+</script>
+
 <template>
   <h1>Document Writer</h1>
+
+  <div v-if="editor">
+    <button
+      :class="{ 'is-active': editor.isActive('bold') }"
+      @click="editor.chain().focus().toggleBold().run()"
+    >
+      bold
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('italic') }"
+      @click="editor.chain().focus().toggleItalic().run()"
+    >
+      italic
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('strike') }"
+      @click="editor.chain().focus().toggleStrike().run()"
+    >
+      strike
+    </button>
+    <button @click="editor.chain().focus().clearNodes().run()">
+      clear nodes
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('paragraph') }"
+      @click="editor.chain().focus().setParagraph().run()"
+    >
+      paragraph
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+    >
+      h1
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+    >
+      h2
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+    >
+      h3
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+    >
+      h4
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+    >
+      h5
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+      @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+    >
+      h6
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('bulletList') }"
+      @click="editor.chain().focus().toggleBulletList().run()"
+    >
+      bullet list
+    </button>
+    <button
+      :class="{ 'is-active': editor.isActive('orderedList') }"
+      @click="editor.chain().focus().toggleOrderedList().run()"
+    >
+      ordered list
+    </button>
+    <button @click="editor.chain().focus().setHorizontalRule().run()">
+      horizontal rule
+    </button>
+    <button @click="editor.chain().focus().setHardBreak().run()">
+      hard break
+    </button>
+    <button @click="editor.chain().focus().undo().run()">
+      undo
+    </button>
+    <button @click="editor.chain().focus().redo().run()">
+      redo
+    </button>
+  </div>
+
+  <editor-content
+    class="doc-editor"
+    :editor="editor"
+  />
 </template>
+
+<style scoped>
+.doc-editor {
+  background-color: var(--white);
+  padding: 0.5em;
+}
+
+</style>
