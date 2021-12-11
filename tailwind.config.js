8a1148c6c1dd24ab629c1d9b573d527ef8822abc
Sam Edwards <samuel.edwards@techstars.com>
2021-12-10 19:04:31 -0500

    updated packages & tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["./index.html", "/packages/renderer/src/**/*.{vue,js,ts}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "/packages/renderer/src/**/*.{vue,js,ts}"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
