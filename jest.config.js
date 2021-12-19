fb34ce16737dfbe016cad688fad0ef45f597f3fa
ste163 <samtedwards@pm.me>
2021-12-19 12:36:24 -0500

    added type module to jest; added notes for what to test

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
   verbose: true,
   preset: "ts-jest",
   testEnvironment: "jest-environment-node",
-  moduleDirectories: ["node_modules"],
+  moduleDirectories: ["node_modules", "types"],
   transform: {},
 };
---
+++ packages/main/src/api/controllers/typeController.spec.test.ts
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment node
+ */
+
+// getAll throws error
+// can get all projects
+// trying to add project with type in db throws error
+// adding a new type that's not in db throws error
+// can add new type
+// trying to delete a project by id not in db throws error
+// can delete type
