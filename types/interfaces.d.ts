016c51644f4ac3aa3278eaf9ecad58d954f8c403
Sam Edwards <samuel.edwards@techstars.com>
2021-12-27 12:26:03 -0500

    added goal interface; updated data model

--- types/interfaces.d.ts
+++ types/interfaces.d.ts
@@ -1,19 +1,11 @@
 declare module "interfaces" {
-  interface Column {
-    header: string;
-    isActive: boolean;
-    dropZone: string;
-    position: number;
-    width: string; // in px; ie '150px'
-  }
-
   interface Project {
     id?: string;
     title: string;
     description: string;
     typeId: string;
     type?: Type;
-    // goals: Goal[]
+    goals: Goal[];
     completed: boolean;
     archived: boolean;
     dateCreated: Date | null;
@@ -25,9 +17,32 @@ declare module "interfaces" {
     value: string;
   }
 
+  interface Goal {
+    id?: string;
+    basedOnWordCountOrPageCount: "word" | "page";
+    frequencyToRepeat: "daily" | "weekly" | "monthly";
+    daysPerFrequency: number; // might be best to only do daysPerFrequency?
+    proofreadCountsTowardGoal: boolean;
+    editCountsTowardGoal: boolean;
+    revisedCountsTowardsGoal: boolean;
+    completed: boolean;
+    dateCreated: Date | null;
+    dateModified: Date | null;
+  }
+
+  // Potentially move out of interfaces because API may never need to know about these
+  // their info will live in localStorage
   interface DropZone {
     name: string;
     maxWidth: string; // in px; ie: '150px' - limits width of columns
     currentWidth: string; // computed property of all the currently active column widths
+  }
+
+  interface Column {
+    header: string;
+    isActive: boolean;
+    dropZone: string;
+    position: number;
+    width: string; // in px; ie '150px'
   }
 }
