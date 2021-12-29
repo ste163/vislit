bc1abcf1136beb11b9ad8da396cf17a7f38cca0a
Sam Edwards <samuel.edwards@techstars.com>
2021-12-28 19:46:25 -0500

    added get goal by id tests

--- packages/main/src/api/repositories/goal-repository.spec.ts
+++ packages/main/src/api/repositories/goal-repository.spec.ts
@@ -3,18 +3,41 @@ import Database from "../database";
 import GoalRepository from "./goal-repository";
 
 describe("goal-repository", () => {
-  let goalRepository: GoalRepository;
+  let seedGoal: Goal;
   let database: Database;
+  let goalRepository: GoalRepository;
 
   beforeEach(() => {
     const { app } = jest.requireMock("electron");
+    seedGoal = {
+      id: "1",
+      projectId: "2",
+      basedOnWordCountOrPageCount: "word",
+      frequencyToRepeat: "daily",
+      proofreadCountsTowardGoal: true,
+      editCountsTowardGoal: true,
+      revisedCountsTowardsGoal: true,
+    };
     database = new Database(app);
+    database.db.data?.goals.push(seedGoal);
     goalRepository = new GoalRepository(database);
   });
 
-  //   it("returns undefined if no goal by id founds", () => {});
+  it("returns undefined if no goal by id founds", () => {
+    expect(goalRepository.getGoalById("999")).toBeUndefined();
+  });
 
-  //   it("returns goal by id", () => {});
+  it("returns goal by id", () => {
+    expect(goalRepository.getGoalById("1")).toEqual({
+      id: "1",
+      projectId: "2",
+      basedOnWordCountOrPageCount: "word",
+      frequencyToRepeat: "daily",
+      proofreadCountsTowardGoal: true,
+      editCountsTowardGoal: true,
+      revisedCountsTowardsGoal: true,
+    });
+  });
 
   it("returns goal after added to database", () => {
     const goalToAdd: Goal = {
@@ -26,7 +49,7 @@ describe("goal-repository", () => {
       revisedCountsTowardsGoal: true,
     };
     goalRepository.add(goalToAdd);
-    expect(database.db.data?.goals.length).toEqual(1);
+    expect(database.db.data?.goals.length).toEqual(2);
   });
 
   //   it("returns error if goal to delete not found", () => {});
