e3756d59d193729f7db2b8fd72e29101b653ecce
ste163 <samtedwards@pm.me>
2022-01-15 18:01:02 -0500

    renamed view files to two names for eslint to work

--- packages/renderer/src/composables/use-is-sidebar-disabled.ts
+++ packages/renderer/src/composables/use-is-sidebar-disabled.ts
@@ -4,9 +4,7 @@ import { useRoute } from "vue-router";
 
 export default function useIsSidebarDisabled(): ComputedRef<boolean> {
   const route = useRoute();
-  const isSidebarDisabled = computed(() =>
+  return computed(() =>
     route.name === "Welcome" ? true : false
   );
-
-  return isSidebarDisabled;
 }
--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,10 +1,48 @@
 import { createApp } from "vue";
-import App from "./app.vue";
-import router from "/@/router/index";
+import { createRouter, createWebHashHistory } from "vue-router";
+import app from "./app.vue";
+import welcome from "./views/view-welcome.vue";
+import summary from "./views/view-summary.vue"
+import writer from "./views/view-writer.vue"
+import progress from "./views/view-progress.vue"
+import visualizationView from "./views/view-visualization.vue"
 import clickOutside from "./directives/click-outside";
 import "./index.css";
 
-createApp(App)
+const routes = [
+  {
+    path: "/",
+    name: "Welcome",
+    component: welcome,
+  },
+  {
+    path: "/summary/:id",
+    name: "Summary",
+    component: summary,
+  },
+  {
+    path: "/writer/:id",
+    name: "Writer",
+    component: writer
+  },
+  {
+    path: "/progress/:id",
+    name: "Progress",
+    component: progress
+  },
+  {
+    path: "/visualization/:id",
+    name: "Visualization",
+    component: visualizationView
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+});
+
+createApp(app)
   .directive("click-outside", clickOutside)
   .use(router)
   .mount("#app");
