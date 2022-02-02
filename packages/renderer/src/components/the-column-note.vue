5952aa1a48d99a2a3fc49950db88f3b14a1a3ef7
Sam Edwards <samuel.edwards@techstars.com>
2022-02-02 18:06:50 -0500

    basic note column structure added

--- packages/renderer/src/components/the-column-note.vue
+++ packages/renderer/src/components/the-column-note.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+// Do the form first and get that saving, then the list view
+// Then depending on how large it is, move to separate components
+import { ref } from "vue";
+
+const isEditViewOpen = ref<boolean>(false);
+</script>
+
 <template>
-  <div>Notes Column Content</div>
+  <!-- Button that opens Note Form component -->
+  <!-- 2 Column States:
+    1. List View
+    2. Edit View
+  -->
+  <div>
+    <!-- List View -->
+    <div v-if="!isEditViewOpen">
+      <button @click="isEditViewOpen = true">Create Note</button>
+
+      <div>
+        <p>All Saved Notes</p>
+      </div>
+    </div>
+
+    <!-- Edit View -->
+    <div v-else>
+      <button @click="isEditViewOpen = false">Back</button>
+      <form>FORM OPEN!</form>
+    </div>
+  </div>
 </template>
