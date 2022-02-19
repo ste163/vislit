e2ef8c3d1b01ee8d697ba9d4192477569ad97b1f
Sam Edwards <62910490+ste163@users.noreply.github.com>
2022-02-19 16:40:18 -0500

    Update deps (#140)
    * updated dependencies and project to match vite-electron-builder
    
    * updated typecheck scripts
    
    * tailwind config in both root and renderer root

--- packages/renderer/postcss.config.js
+++ packages/renderer/postcss.config.js
@@ -1,4 +1,3 @@
-// Needs to be in root and in /packages/renderer
 module.exports = {
   plugins: {
     tailwindcss: {},
--- packages/renderer/tailwind.config.js
+++ packages/renderer/tailwind.config.js
@@ -1,6 +1,5 @@
-// Needs to be in root and in /packages/renderer
 module.exports = {
-  content: ["./index.html", "./packages/renderer/src/**/*.{vue,js,ts}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
