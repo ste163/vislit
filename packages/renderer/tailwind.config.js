4106d1a69961d40e4300cab755bf4efb05500b7b
ste163 <samtedwards@pm.me>
2022-02-25 14:34:20 -0500

    moved icons to new dir; setup colors in tailwind

--- packages/renderer/tailwind.config.js
+++ packages/renderer/tailwind.config.js
@@ -1,8 +1,25 @@
+const colors = require("tailwindcss/colors");
+
+// Must be in ./ and in ./packages/renderer
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  content: ["./index.html", "./packages/renderer/src/**/*.{vue,js,ts}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Heebo"],
+      serif: ["Noto"],
+    },
+    extend: {
+      colors: {
+        currentColor: colors.currentColor,
+        currentFill: colors.currentFill,
+        primary: "#3772ff",
+        alert: "#b24c63",
+        info: "#fdca40",
+        white: colors.white,
+        black: "#333333",
+      },
+    },
   },
   variants: {
     extend: {},
