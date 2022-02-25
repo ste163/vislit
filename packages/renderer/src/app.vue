2ef330425fd23fc34906ccaa36ef3309b41dcdf9
ste163 <samtedwards@pm.me>
2022-02-25 13:13:51 -0500

    added loading examples; sidebar spec comments

--- packages/renderer/src/app.vue
+++ packages/renderer/src/app.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
+import TheSidebar from "./components/the-sidebar.vue";
 
 onMounted(async () => {
-  // Show loading
+  // isLoading is true by default
   // fetch Projects
   // fetch Types
   // if no projects, show welcome view
-  // otherwise, read localStorage
+  // // route to '/'
+  // // stop loading
+  // else,
+  // read localStorage
   // if it's got a last opened project, open that project
-  // if there is no last opened project
-  // open most recent
+  // if there is no last opened project, open most recent
+  // route to /project/:id
   // stop loading
 });
 </script>
@@ -17,11 +21,9 @@ onMounted(async () => {
 <template>
   <the-sidebar />
 
-  <div>
-    <main>
-      <router-view v-slot="{ Component }">
-        <component :is="Component" />
-      </router-view>
-    </main>
-  </div>
+  <main>
+    <router-view v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
+  </main>
 </template>
---
+++ packages/renderer/src/components/this-sidebar.spec.ts
@@ -0,0 +1,5 @@
+// Possible test scenarios:
+// Might be emitting events on click (to open columns)
+// Router routes properly when the view buttons clicked
+// and that the sidebar is disabled based on passed in props (ie, if there is no activeProject or projectId)
+// passed in, disable the sidebar
