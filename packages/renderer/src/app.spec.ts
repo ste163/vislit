490eec53d08e57319975e3a1d5cb522325a4f6e2
ste163 <samtedwards@pm.me>
2022-02-26 14:28:23 -0500

    added notes for app tests

---
+++ packages/renderer/src/app.spec.ts
@@ -0,0 +1,13 @@
+import { it } from "vitest";
+// Fetching data fails:
+// render failure state
+// fetching data successfully on user with no data
+// show welcome page
+// user with project but no localStorage,
+// route to /summary with most recent project
+// user with data and localStorage settings
+// restore settings
+
+it.skip(
+  "when data (projects or types) fails to fetch, render error page and no sidebar"
+);
