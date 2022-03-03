7c7dfc12041cc6bf63391245214137e2d57a8cd2
Sam Edwards <samuel.edwards@techstars.com>
2022-03-02 19:19:38 -0500

    added button test outlines; added icon setup

---
+++ packages/renderer/src/components/base-button.spec.ts
@@ -0,0 +1,7 @@
+import { it } from "vitest";
+// if isDisabled is true, unable to click and colors are default
+// if submitting, show spinner + disable button, regardless of prop
+// if icon is passed in and then isSubmitting becomes true, stop rendering icon, and render spinner
+// // then re-render icon when isSubmitting is finished
+
+it.skip("renders loading spinner when isSubmitting is true and button becomes disabled regardless of prop", () => {});
